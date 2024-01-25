bfff6e79f47f0d5c83678d1cc8a4b66aeb27ff2e
Parth Raut <praut@umich.edu>
2024-01-25 13:46:04 -0500

    finished code

--- src/main.py
+++ src/main.py
@@ -11,12 +11,17 @@ app = Flask(__name__)
 
 @app.errorhandler(400)
 def handle_bad_request(e):
-    # Check if the error message is from abort(400) and is a string
+    """Check if the error message is from abort(400) and is a string. Forms
+    a json response with the error message and returns it with status code 400."""
+
     if isinstance(e.description, str):
         return jsonify(error=e.description), 400
     return jsonify(error="Bad request"), 400
 
 def check_welformed(data):
+    """Checks if the request is well formed as described in register.json. 
+    If not, aborts with status code 400."""
+
     if 'message' not in data:
         abort(400, "Message not specified")
     if 'model' not in data:
@@ -32,30 +37,32 @@ def check_welformed(data):
     
 
 def inference(data):
+    """Sends a POST request to the llama2 service with the data. Returns the response."""
+
+    # check if request is well formed
     check_welformed(data)
 
     try:
-        if data['model'] == "llama2-7b":
-            # run model with message and return response
-            # response = requests.post("http://llama2:5000/inference", json=data)
-            print("sending request to llama2")
-            response = requests.post("http://127.0.0.1:5000/inference", json=data)
-
-            if response.status_code == 200:
+        # send request to llama2 service
+        response = requests.post(registery[data['model']]["endpoint"], json=data)
+        if response.status_code == 200:
                 return response.json(), 200
-            else:
-                return {"error": "Failed to get response from llama2 service"}, response.status_code
+        else:
+            return {"error": "Failed to get response from llama2 service"}, response.status_code
     except requests.exceptions.ConnectionError:
         return {"error": "Unable to connect to llama2 service"}, 503  # 503 Service Unavailable
     
 
 @app.route('/completion', methods=['POST'])
 def completion():
+    """Handles POST requests to /completion. Returns the response."""
+
     data = request.json
 
+    # run inference on request
     response, status_code = inference(data)
 
-    print(f"recieved request to llama2{response}")
+    # return response, status code to client
     return response, status_code
 
 if __name__ == "__main__":
--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,5 @@
 # local: 
-gunicorn -w 2 -b 127.0.0.1:3000 main:app --timeout 300
+gunicorn -w 16 -b 127.0.0.1:3000 main:app --timeout 500
 
 # docker:
 # gunicorn -w 2 -b 0.0.0.0:3000 main:app --timeout 300
